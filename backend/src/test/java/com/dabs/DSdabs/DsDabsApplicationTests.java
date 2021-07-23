14788447954cfd8d28eb8e23693adae3cb5ba68c
Diego Alessandro <diego_alessandro@hotmail.com>
2021-07-23 09:28:38 -0300

    Projeto criado

---
+++ backend/src/test/java/com/dabs/DSdabs/DsDabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabs.DSdabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
